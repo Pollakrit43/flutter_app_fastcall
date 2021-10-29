d813915a153aa35abf35ca1e2fd0bb319b775ab4
s6252410020@sau.ac.th <s6252410020@sau.ac.th>
2021-10-29 12:30:10 +0700

    Code V.2: ListFood

--- lib/views/listfood_new_ui.dart
+++ lib/views/listfood_new_ui.dart
@@ -3,13 +3,12 @@ import 'package:flutter_app_fastcall/models/foodlist.dart';
 import 'package:flutter_app_fastcall/views/listfood_detail_ui.dart';
 
 class ListfoodNewUI extends StatefulWidget {
-  const ListfoodNewUI({Key? key}) : super(key: key);
-
   @override
   _ListfoodNewUIState createState() => _ListfoodNewUIState();
 }
 
 class _ListfoodNewUIState extends State<ListfoodNewUI> {
+  @override
   List<FoodList> foodList = [
     FoodList(
       name: 'Chesters Grill',
@@ -17,6 +16,8 @@ class _ListfoodNewUIState extends State<ListfoodNewUI> {
       facebook: 'chesterthai',
       mobile: '1145',
       image: 'f1.jpg',
+      pikat:
+          'https://www.google.com/maps/place/Chesters+Grill/@13.7047008,100.367133,12z',
     ),
     FoodList(
       name: 'Narai Pizzeria',
@@ -24,6 +25,8 @@ class _ListfoodNewUIState extends State<ListfoodNewUI> {
       facebook: 'naraipizzeria',
       mobile: '1744',
       image: 'f2.jpg',
+      pikat:
+          'https://www.google.com/maps/place/Narai+Pizzeria/@13.7046811,100.3671329,12z',
     ),
     FoodList(
       name: 'S&P',
@@ -31,6 +34,8 @@ class _ListfoodNewUIState extends State<ListfoodNewUI> {
       facebook: 'snpfood',
       mobile: '1344',
       image: 'f3.jpg',
+      pikat:
+          'https://www.google.com/maps/place/S%26P+(Seacon+Bangkae)/@13.7046614,100.3671328,12z',
     ),
     FoodList(
       name: 'Oishi',
@@ -38,6 +43,8 @@ class _ListfoodNewUIState extends State<ListfoodNewUI> {
       facebook: 'oishifoodstation',
       mobile: '1773',
       image: 'f4.jpg',
+      pikat:
+          'https://www.google.com/maps/search/Oishi/@13.7046417,100.3671327,12z',
     ),
     FoodList(
       name: 'MK Restaurants',
@@ -45,6 +52,8 @@ class _ListfoodNewUIState extends State<ListfoodNewUI> {
       facebook: 'mkrestaurants',
       mobile: '02-248-5555',
       image: 'f5.jpg',
+      pikat:
+          'https://www.google.com/maps/search/MK+Restaurants/@13.704622,100.3671326,12z',
     ),
     FoodList(
       name: 'The Pizza',
@@ -52,6 +61,8 @@ class _ListfoodNewUIState extends State<ListfoodNewUI> {
       facebook: 'thepizzacompany',
       mobile: '1112',
       image: 'f6.jpg',
+      pikat:
+          'https://www.google.com/maps/search/The+Pizza/@13.7046023,100.3671325,12z',
     ),
     FoodList(
       name: 'Mc Donals',
@@ -59,6 +70,8 @@ class _ListfoodNewUIState extends State<ListfoodNewUI> {
       facebook: 'McThai',
       mobile: '1711',
       image: 'f7.jpg',
+      pikat:
+          'https://www.google.com/maps/search/Mc+Donals/@13.7045826,100.3671324,12z',
     ),
     FoodList(
       name: 'Pizza Hut',
@@ -66,6 +79,8 @@ class _ListfoodNewUIState extends State<ListfoodNewUI> {
       facebook: 'pizzahutthailand',
       mobile: '1150',
       image: 'f8.jpg',
+      pikat:
+          'https://www.google.com/maps/search/Pizza+Hut/@13.7045629,100.3671323,12z',
     ),
     FoodList(
       name: 'KFC',
@@ -73,6 +88,8 @@ class _ListfoodNewUIState extends State<ListfoodNewUI> {
       facebook: 'kfcth',
       mobile: '1150',
       image: 'f9.jpg',
+      pikat:
+          'https://www.google.com/maps/search/KFC/@13.7045432,100.3671321,12z',
     ),
     FoodList(
       name: 'JJ Delivery',
@@ -80,6 +97,8 @@ class _ListfoodNewUIState extends State<ListfoodNewUI> {
       facebook: 'jjdelivery',
       mobile: '02-712-3000',
       image: 'f10.jpg',
+      pikat:
+          'https://www.google.com/maps/place/JJ+Delivery+Service/@13.7239672,100.5763232,17z',
     ),
     FoodList(
       name: 'Burger King',
@@ -87,6 +106,8 @@ class _ListfoodNewUIState extends State<ListfoodNewUI> {
       facebook: 'burgerkingthailand',
       mobile: '1112',
       image: 'f11.jpg',
+      pikat:
+          'https://www.google.com/maps/search/Burger+King/@13.723967,100.5697571,15z',
     ),
     FoodList(
       name: 'See Fah',
@@ -94,6 +115,8 @@ class _ListfoodNewUIState extends State<ListfoodNewUI> {
       facebook: 'seefahfanpage',
       mobile: '02-800-8080',
       image: 'f12.jpg',
+      pikat:
+          'https://www.google.com/maps/search/See+Fah/@13.7239664,100.5697571,15z',
     ),
     FoodList(
       name: 'ฮองมิน',
@@ -101,6 +124,8 @@ class _ListfoodNewUIState extends State<ListfoodNewUI> {
       facebook: 'hongminfanpage',
       mobile: '02-248-0123',
       image: 'f13.jpg',
+      pikat:
+          'https://www.google.com/maps/search/%E0%B8%AE%E0%B8%AD%E0%B8%87%E0%B8%A1%E0%B8%B4%E0%B8%99/@13.7239638,100.5434924,13z',
     ),
     FoodList(
       name: 'Yoshinoya',
@@ -108,6 +133,8 @@ class _ListfoodNewUIState extends State<ListfoodNewUI> {
       facebook: 'YoshinoyaThailand',
       mobile: '02-663-3888',
       image: 'f14.jpg',
+      pikat:
+          'https://www.google.com/maps/search/Yoshinoya/@13.7239589,100.5434923,13z',
     ),
     FoodList(
       name: 'ฮั่วเซ่งฮง',
@@ -115,6 +142,8 @@ class _ListfoodNewUIState extends State<ListfoodNewUI> {
       facebook: 'huasenghong',
       mobile: '02-2480123',
       image: 'f15.jpg',
+      pikat:
+          'https://www.google.com/maps/search/%E0%B8%AE%E0%B8%B1%E0%B9%88%E0%B8%A7%E0%B9%80%E0%B8%8B%E0%B9%88%E0%B8%87%E0%B8%AE%E0%B8%87/@13.7239539,100.5434923,13z',
     ),
     FoodList(
       name: 'Scoozi Pizza',
@@ -122,6 +151,8 @@ class _ListfoodNewUIState extends State<ListfoodNewUI> {
       facebook: 'scoozipizzaclub',
       mobile: '02-715-8555',
       image: 'f16.jpg',
+      pikat:
+          'https://www.google.com/maps/search/Scoozi+Pizza/@13.723949,100.5434923,13z',
     ),
     FoodList(
       name: 'โดมิโน่ พิซซ่า',
@@ -129,24 +160,25 @@ class _ListfoodNewUIState extends State<ListfoodNewUI> {
       facebook: 'DominosPizzaThailand',
       mobile: '1612',
       image: 'f17.jpg',
+      pikat:
+          'https://www.google.com/maps/search/%E0%B9%82%E0%B8%94%E0%B8%A1%E0%B8%B4%E0%B9%82%E0%B8%99%E0%B9%88+%E0%B8%9E%E0%B8%B4%E0%B8%8B%E0%B8%8B%E0%B9%88%E0%B8%B2/@13.7239441,100.5434923,13z',
     ),
   ];
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.green[50],
       appBar: AppBar(
         title: Text(
-          'Food List New',
+          'Food List New ร้านยอดฮิต',
         ),
         centerTitle: true,
-        backgroundColor: Colors.green,
+        backgroundColor: Colors.green[600],
       ),
       body: Column(
         children: [
           Container(
             width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height * 0.3,
+            height: MediaQuery.of(context).size.height * 0.2,
             child: Image.asset(
               'assets/images/fastfood.jpg',
               fit: BoxFit.cover,
@@ -156,21 +188,27 @@ class _ListfoodNewUIState extends State<ListfoodNewUI> {
             child: ListView.separated(
               separatorBuilder: (context, index) {
                 return Divider(
-                  height: 10,
-                  color: Colors.black,
+                  height: 10.0,
+                  color: Colors.green[200],
                 );
               },
               itemCount: foodList.length,
               itemBuilder: (context, index) {
                 return ListTile(
                   onTap: () {
-                    // เปิดไปหน้า ListfoodDetailUI
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (contex) {
+                        builder: (context) {
+                          //ที่หน้า ListfoodDetailUI มีการสร้างตัวแปรรับค่า
+                          //ดังนั้นในการเปิดไปหน้า ListfoodDetailUI เราจึงต้องส่งข่้อมูลไปด้วย
                           return ListfoodDetailUI(
-                            foodListDetail: foodList[index],
+                            name: foodList[index].name,
+                            website: foodList[index].website,
+                            facebook: foodList[index].facebook,
+                            mobile: foodList[index].mobile,
+                            image: foodList[index].image,
+                            pikat: foodList[index].pikat,
                           );
                         },
                       ),
